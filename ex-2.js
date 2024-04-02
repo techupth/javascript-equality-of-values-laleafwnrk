0821db5382997a8b607868fa2a043a334ba0cacf
Wanarak <wanarak.r@gmail.com>
2024-04-02 19:21:26 +0700

    Assignment : JavaScript Equality of Values Assignment

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; // False
+null == undefined; // Ture
+10 == "10"; // False
+[10] == 10; // True
+[1,2] == "1,2"; //False
